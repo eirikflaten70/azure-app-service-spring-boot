7c67f66aa33543a5a4ce4a04f6ac398414988f62
sreejith sasidharan <sreejith.sasidharan@noria.no>
2022-05-31 14:31:33 +0200

    environment vars

--- src/main/java/noaria/no/hellospring/resource/HelloController.java
+++ src/main/java/noaria/no/hellospring/resource/HelloController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import noaria.no.hellospring.model.Greeting;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class HelloController {
+
+  
+  @Value("${environment.jms-queues.receipts-in}")
+  private String receiptInQueue;
   
   public String index() {
     return "Greetings from Azure Spring Apps!";
@@ -18,6 +23,7 @@ public class HelloController {
   public Greeting get() {
     Greeting greeting = new Greeting();
     greeting.setName("Greetings to Hello World");
+    System.out.println(receiptInQueue);
     return greeting;
   }
 
